c47d7c9460064efd40cd21b567f3bc6826705eea
danielFernandezJd <mdanif9728@gmail.com>
2024-04-03 19:59:40 -0700

    Saving

--- public/app.js
+++ public/app.js
@@ -1,20 +1,28 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
+console.log('Hello')
+
+// Create map:                                                       
+const map = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
 
-// Create map
-
-
-
-
-// Add openstreetmap tiles
-
-
+// Add OpenStreetMap tiles:
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
 
 
-// Create and add a geolocation marker
+
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(map).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
+
 
 
 
